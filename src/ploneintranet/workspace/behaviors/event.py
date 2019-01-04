b49008a3d64206384c75dcfe6a2700b7b4ba0e48
Martin Peeters <martin.peeters@affinitic.be>
2019-01-04 18:06:32 +0100

    Fix an issue with adding events directly on the general calendar app

--- src/ploneintranet/workspace/behaviors/event.py
+++ src/ploneintranet/workspace/behaviors/event.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from plone.app.contenttypes.interfaces import IEvent
+from plone.autoform import directives
 from plone.autoform.interfaces import IFormFieldProvider
+from z3c.form.browser.multi import multiFieldWidgetFactory
 from zope import schema
 from zope.interface import alsoProvides
 
@@ -8,11 +10,13 @@ from zope.interface import alsoProvides
 class IPloneIntranetEvent(IEvent):
     """Additional fields for PloneIntranet events
     """
+    directives.widget(agenda_items=multiFieldWidgetFactory)
     agenda_items = schema.List(
         title=u"Agenda Items",
         description=u"Items associated with this event",
         required=False,
         default=[],
+        value_type=schema.TextLine(title=u'Agenda item'),
     )
 
     organizer = schema.TextLine(
